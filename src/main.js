101e93ac43777736a63b824a744d829c299602a6
leonardo-santoz <leonardo.cienciacomputer@gmail.com>
2020-11-04 00:36:53 -0300

    nome das props e case insensitive no DOM

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+
+import FilmesListaIten from './components/FilmesListaIten'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  components: {
+    FilmesListaIten
+  }
 }).$mount('#app')
